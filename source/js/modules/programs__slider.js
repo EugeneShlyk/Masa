d33a37743c93ecd6da20a207b24b6ce4b4de05f9
Evgene Shlyk <serpupstars@gmail.com>
2023-09-12 21:13:15 +1000

    вторник вечер

--- source/js/modules/programs__slider.js
+++ source/js/modules/programs__slider.js
@@ -41,3 +41,22 @@ export const initProgramsSlider = () =>
       },
     },
   });
+
+export const focusSlider = (iSlider, swiper) => {
+  const links = iSlider.querySelectorAll('a');
+
+  const func = function (evt) {
+    const slides = iSlider.querySelectorAll('.swiper-slide');
+    const iTarget = evt.target.closest('.swiper-slide');
+    const index = Array.from(slides).indexOf(iTarget);
+    swiper.slideTo(index);
+  };
+
+  links.forEach((
+    (link) => {
+      links.length && link.addEventListener('focus', func);
+    }
+  ));
+};
+
+focusSlider(slider, Swiper);
